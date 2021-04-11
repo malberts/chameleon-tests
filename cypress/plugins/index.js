c5006fd61057a9248b0b0e81405639a00bd2c784
Morne Alberts <morne@malberts.net>
2021-04-11 17:43:01 +0200

    Add breakpoints as env variable

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,8 +1,12 @@
+const breakpoints = require('../fixtures/screens.json')
 const getCompareSnapshotsPlugin = require('cypress-visual-regression/dist/plugin');
 
 /**
  * @type {Cypress.PluginConfig}
  */
-module.exports = (on, config) => {
+module.exports = async (on, config) => {
+  config.env.breakpoints = breakpoints
+  console.log('breakpoints', breakpoints)
   getCompareSnapshotsPlugin(on, config);
+  return config
 }
