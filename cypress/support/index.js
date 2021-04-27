57b48cff87cce879b25bb881ca575af2a8eba8dd
Morne Alberts <morne@malberts.net>
2021-04-27 12:29:16 +0200

    Add initial report screenshots

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -1,2 +1,69 @@
+const path = require('path')
+const addContext = require('mochawesome/addContext')
+
 import './commands'
 import './assertions'
+
+// Add screenshots to Mochawesome report.
+const screenshotsFolder = Cypress.config('screenshotsFolder')
+const baseFolder = Cypress.env('SNAPSHOT_BASE_DIRECTORY')
+const diffFolder = Cypress.env('SNAPSHOT_DIFF_DIRECTORY')
+
+Cypress.Screenshot.defaults({
+  onAfterScreenshot(_el, details) {
+    if (!details.path) {
+      return
+    }
+
+    cy.once('test:after:run', (test) => {
+      const normalizedScreenshotPath =
+        'screenshots' + details.path.replace(screenshotsFolder, '')
+
+      addContext(
+        { test },
+        {
+          title: normalizedScreenshotPath.includes('(failed)')
+            ? 'Failed screenshot'
+            : 'Actual',
+          value: normalizedScreenshotPath,
+        }
+      )
+
+      // Add diff and expected for normal screenshots.
+      if (!normalizedScreenshotPath.includes('(failed)')) {
+        const basePath = path.join(
+          baseFolder,
+          details.specName,
+          `${details.name}.png`.replace('-actual', '-base')
+        )
+        const diffPath = path.join(
+          diffFolder,
+          details.specName,
+          `${details.name}.png`.replace('-actual', '-diff')
+        )
+        const normalizeBasePath = basePath.replace(
+          'screenshots' + baseFolder,
+          ''
+        )
+        const normalizeDiffPath =
+          'screenshots' + diffPath.replace(diffFolder, '')
+
+        addContext(
+          { test },
+          {
+            title: 'Diff',
+            value: normalizeDiffPath,
+          }
+        )
+
+        addContext(
+          { test },
+          {
+            title: 'Expected',
+            value: normalizeBasePath,
+          }
+        )
+      }
+    })
+  },
+})
