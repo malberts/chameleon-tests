bce88ab7f4d5ff8d96a1f1bb48ac3fbbfbc7847a
Morne Alberts <morne@malberts.net>
2021-04-28 22:42:42 +0200

    Separate Typography tests

--- cypress/integration/bootstrap/Typography.js
+++ cypress/integration/bootstrap/Typography.js
@@ -16,10 +16,40 @@ describe('Bootstrap: Typography', () => {
         cy.get('#bootstrap-version').should('not.be.empty')
       })
 
-      it('Typography', () => {
-        cy.get('.bs-docs-section:nth-of-type(3)').within((section) => {
-          cy.wrap(section).compareSnapshot(`Typography_${breakpoint.name}`)
-        })
+      it('Typography: Heading', () => {
+        cy.get(
+          '.bs-docs-section:nth-of-type(3) > div:nth-child(2) > div:nth-child(1) > .bs-component'
+        ).compareSnapshot(`Typography_Heading_${breakpoint.name}`)
+      })
+
+      it('Typography: Body', () => {
+        cy.get(
+          '.bs-docs-section:nth-of-type(3) > div:nth-child(2) > div:nth-child(2) > .bs-component'
+        ).compareSnapshot(`Typography_Body_${breakpoint.name}`)
+      })
+
+      it('Typography: Emphasis', () => {
+        cy.get(
+          '.bs-docs-section:nth-of-type(3) > div:nth-child(2) > div:nth-child(3) > .bs-component'
+        ).compareSnapshot(`Typography_Emphasis_${breakpoint.name}`)
+      })
+
+      it('Typography: Blockquotes: Left', () => {
+        cy.get(
+          '.bs-docs-section:nth-of-type(3) > div:nth-child(4) > div:nth-child(1) > .bs-component'
+        ).compareSnapshot(`Typography_Blockquotes_Left_${breakpoint.name}`)
+      })
+
+      it('Typography: Blockquotes: Middle', () => {
+        cy.get(
+          '.bs-docs-section:nth-of-type(3) > div:nth-child(4) > div:nth-child(2) > .bs-component'
+        ).compareSnapshot(`Typography_Blockquotes_Middle_${breakpoint.name}`)
+      })
+
+      it('Typography: Blockquotes: Right', () => {
+        cy.get(
+          '.bs-docs-section:nth-of-type(3) > div:nth-child(4) > div:nth-child(3) > .bs-component'
+        ).compareSnapshot(`Typography_Blockquotes_Right_${breakpoint.name}`)
       })
     })
   })
