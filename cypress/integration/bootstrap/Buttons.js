adb3bf666d6e51992c15c742ee4f09627557ca90
Morne Alberts <morne@malberts.net>
2021-04-28 22:15:25 +0200

    Separate Buttons tests

--- cypress/integration/bootstrap/Buttons.js
+++ cypress/integration/bootstrap/Buttons.js
@@ -16,13 +16,31 @@ describe('Bootstrap: Buttons', () => {
         cy.get('#bootstrap-version').should('not.be.empty')
       })
 
-      it('Buttons', () => {
-        cy.get('.bs-docs-section:nth-of-type(2)').compareSnapshot(
-          `Buttons_${breakpoint.name}`
-        )
+      it('Buttons: Normal', () => {
+        cy.get(
+          '.bs-docs-section:nth-of-type(2) > div > div:nth-child(1) > .bs-component:nth-child(1)'
+        ).compareSnapshot(`Buttons_Normal_${breakpoint.name}`)
       })
 
-      it('Buttons Dropdown', () => {
+      it('Buttons: Disabled', () => {
+        cy.get(
+          '.bs-docs-section:nth-of-type(2) > div > div:nth-child(1) > .bs-component:nth-child(2)'
+        ).compareSnapshot(`Buttons_Disabled_${breakpoint.name}`)
+      })
+
+      it('Buttons: Outline', () => {
+        cy.get(
+          '.bs-docs-section:nth-of-type(2) > div > div:nth-child(1) > .bs-component:nth-child(3)'
+        ).compareSnapshot(`Buttons_Outline_${breakpoint.name}`)
+      })
+
+      it('Buttons: Dropdown', () => {
+        cy.get(
+          '.bs-docs-section:nth-of-type(2) > div > div:nth-child(1) > .bs-component:nth-child(4)'
+        ).compareSnapshot(`Buttons_Dropdown${breakpoint.name}`)
+      })
+
+      it('Buttons: Dropdown interaction', () => {
         cy.get('.bs-docs-section:nth-of-type(2)').within((section) => {
           cy.get('#btnGroupDrop1').click()
           cy.get('[aria-labelledby="btnGroupDrop1"]')
@@ -34,6 +52,48 @@ describe('Bootstrap: Buttons', () => {
             .contains('Dropdown link')
             .should('not.be.visible')
         })
+      })
+
+      it('Buttons: Sizes', () => {
+        cy.get(
+          '.bs-docs-section:nth-of-type(2) > div > div:nth-child(1) > .bs-component:nth-child(5)'
+        ).compareSnapshot(`Buttons_Sizes_${breakpoint.name}`)
+      })
+
+      it('Buttons: Block', () => {
+        cy.get(
+          '.bs-docs-section:nth-of-type(2) > div > div:nth-child(2) > .bs-component:nth-child(1)'
+        ).compareSnapshot(`Buttons_Block_${breakpoint.name}`)
+      })
+
+      it('Buttons: Check', () => {
+        cy.get(
+          '.bs-docs-section:nth-of-type(2) > div > div:nth-child(2) > .bs-component:nth-child(2)'
+        ).compareSnapshot(`Buttons_Check_${breakpoint.name}`)
+      })
+
+      it('Buttons: Radio', () => {
+        cy.get(
+          '.bs-docs-section:nth-of-type(2) > div > div:nth-child(2) > .bs-component:nth-child(3)'
+        ).compareSnapshot(`Buttons_Radio_${breakpoint.name}`)
+      })
+
+      it('Buttons: Vertical', () => {
+        cy.get(
+          '.bs-docs-section:nth-of-type(2) > div > div:nth-child(2) > .bs-component:nth-child(4)'
+        ).compareSnapshot(`Buttons_Vertical_${breakpoint.name}`)
+      })
+
+      it('Buttons: Group', () => {
+        cy.get(
+          '.bs-docs-section:nth-of-type(2) > div > div:nth-child(2) > .bs-component:nth-child(5)'
+        ).compareSnapshot(`Buttons_Group_${breakpoint.name}`)
+      })
+
+      it('Buttons: Toolbar', () => {
+        cy.get(
+          '.bs-docs-section:nth-of-type(2) > div > div:nth-child(2) > .bs-component:nth-child(6)'
+        ).compareSnapshot(`Buttons_Toolbar_${breakpoint.name}`)
       })
     })
   })
