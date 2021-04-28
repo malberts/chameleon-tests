301bf0d0d626c28b2b9bcff017b1cc678ddd5d33
Morne Alberts <morne@malberts.net>
2021-04-28 23:04:37 +0200

    Separate Nav tests

--- cypress/integration/bootstrap/Navs.js
+++ cypress/integration/bootstrap/Navs.js
@@ -16,57 +16,92 @@ describe('Bootstrap: Navs', () => {
         cy.get('#bootstrap-version').should('not.be.empty')
       })
 
-      it('Navs', () => {
+      it('Navs: Tabs', () => {
+        cy.get(
+          '.bs-docs-section:nth-of-type(6) > div:nth-child(2) > div:nth-child(1) > .bs-component'
+        ).compareSnapshot(`Navs_Tabs_${breakpoint.name}`)
+      })
+
+      it('Navs: Pills: Horizontal', () => {
+        cy.get(
+          '.bs-docs-section:nth-of-type(6) > div:nth-child(2) > div:nth-child(2) > .bs-component:nth-of-type(1)'
+        ).compareSnapshot(`Navs_Pills_Horizontal_${breakpoint.name}`)
+      })
+
+      it('Navs: Pills: Vertical', () => {
+        cy.get(
+          '.bs-docs-section:nth-of-type(6) > div:nth-child(2) > div:nth-child(2) > .bs-component:nth-of-type(2)'
+        ).compareSnapshot(`Navs_Pills_Vertical_${breakpoint.name}`)
+      })
+
+      it('Navs: Tabs: Switch', () => {
         cy.get('.bs-docs-section:nth-of-type(6)').within((section) => {
-          cy.wrap(section).compareSnapshot(`Navs_${breakpoint.name}`)
+          // Home tab
+          cy.get('[href="#home"]').click()
+          cy.get('#myTabContent #home')
+            .should('be.visible')
+            .contains('Raw denim')
+          // Profile tab
+          cy.get('[href="#profile"]').click()
+          cy.get('#myTabContent #profile')
+            .should('be.visible')
+            .contains('Food truck')
         })
       })
 
-      it('Navs Dropdown', () => {
+      it('Navs: Tabs: Dropdown', () => {
         cy.get('.bs-docs-section:nth-of-type(6)').within((section) => {
-          // Tabs
           cy.get('.nav-tabs').within(() => {
+            // Open
             cy.get('[data-toggle="dropdown"]').click()
             cy.get('.dropdown-menu')
               .should('be.visible')
               .contains('Action')
               .should('be.visible')
-
-            cy.get('[data-toggle="dropdown"]').click()
-            cy.get('.dropdown-menu')
-              .should('not.be.visible')
-              .contains('Action')
-              .should('not.be.visible')
-          })
-
-          // Pills - horizontal
-          cy.get('.nav-pills:not(.flex-column)').within(() => {
-            cy.get('[data-toggle="dropdown"]').click()
-            cy.get('.dropdown-menu')
-              .should('be.visible')
-              .contains('Action')
-              .should('be.visible')
-
+            // Close
             cy.get('[data-toggle="dropdown"]').click()
             cy.get('.dropdown-menu')
               .should('not.be.visible')
               .contains('Action')
               .should('not.be.visible')
           })
+        })
 
-          // Pills - vertical
-          cy.get('.nav-pills.flex-column').within(() => {
-            cy.get('[data-toggle="dropdown"]').click()
-            cy.get('.dropdown-menu')
-              .should('be.visible')
-              .contains('Action')
-              .should('be.visible')
+        it('Navs: Pills: Horizontal: Dropdown', () => {
+          cy.get('.bs-docs-section:nth-of-type(6)').within((section) => {
+            cy.get('.nav-pills:not(.flex-column)').within(() => {
+              // Open
+              cy.get('[data-toggle="dropdown"]').click()
+              cy.get('.dropdown-menu')
+                .should('be.visible')
+                .contains('Action')
+                .should('be.visible')
+              // Close
+              cy.get('[data-toggle="dropdown"]').click()
+              cy.get('.dropdown-menu')
+                .should('not.be.visible')
+                .contains('Action')
+                .should('not.be.visible')
+            })
+          })
+        })
 
-            cy.get('[data-toggle="dropdown"]').click()
-            cy.get('.dropdown-menu')
-              .should('not.be.visible')
-              .contains('Action')
-              .should('not.be.visible')
+        it('Navs: Pills: Vertical: Dropdown', () => {
+          cy.get('.bs-docs-section:nth-of-type(6)').within((section) => {
+            cy.get('.nav-pills.flex-column').within(() => {
+              // Open
+              cy.get('[data-toggle="dropdown"]').click()
+              cy.get('.dropdown-menu')
+                .should('be.visible')
+                .contains('Action')
+                .should('be.visible')
+              // Close
+              cy.get('[data-toggle="dropdown"]').click()
+              cy.get('.dropdown-menu')
+                .should('not.be.visible')
+                .contains('Action')
+                .should('not.be.visible')
+            })
           })
         })
       })
