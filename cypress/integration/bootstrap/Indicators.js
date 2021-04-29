575aa7f141bf1b0ff68b4b43f1b59e9a4bf1d833
Morne Alberts <morne@malberts.net>
2021-04-29 09:36:39 +0200

    Split Indicator tests

--- cypress/integration/bootstrap/Indicators.js
+++ cypress/integration/bootstrap/Indicators.js
@@ -16,9 +16,57 @@ describe('Bootstrap: Indicators', () => {
         cy.get('#bootstrap-version').should('not.be.empty')
       })
 
-      it('Indicators', () => {
-        cy.get('.bs-docs-section:nth-of-type(7)').compareSnapshot(
-          `Indicators_${breakpoint.name}`
+      it('Indicators: Alerts: Warning', () => {
+        cy.get('.bs-docs-section:nth-of-type(7) > div:nth-child(2) > div:nth-child(1) .bs-component').compareSnapshot(
+          `Indicators_Alert_Warning_${breakpoint.name}`
+        )
+      })
+
+      it('Indicators: Alerts: Danger', () => {
+        cy.get('.bs-docs-section:nth-of-type(7) > div:nth-child(3) > div:nth-child(1) .bs-component').compareSnapshot(
+          `Indicators_Alert_Danger_${breakpoint.name}`
+        )
+      })
+
+      it('Indicators: Alerts: Success', () => {
+        cy.get('.bs-docs-section:nth-of-type(7) > div:nth-child(3) > div:nth-child(2) .bs-component').compareSnapshot(
+          `Indicators_Alert_Success_${breakpoint.name}`
+        )
+      })
+
+      it('Indicators: Alerts: Info', () => {
+        cy.get('.bs-docs-section:nth-of-type(7) > div:nth-child(3) > div:nth-child(3) .bs-component').compareSnapshot(
+          `Indicators_Alert_Info_${breakpoint.name}`
+        )
+      })
+
+      it('Indicators: Alerts: Primary', () => {
+        cy.get('.bs-docs-section:nth-of-type(7) > div:nth-child(4) > div:nth-child(1) .bs-component').compareSnapshot(
+          `Indicators_Alert_Primary_${breakpoint.name}`
+        )
+      })
+
+      it('Indicators: Alerts: Secondary', () => {
+        cy.get('.bs-docs-section:nth-of-type(7) > div:nth-child(4) > div:nth-child(2) .bs-component').compareSnapshot(
+          `Indicators_Alert_Secondary_${breakpoint.name}`
+        )
+      })
+
+      it('Indicators: Alerts: Light', () => {
+        cy.get('.bs-docs-section:nth-of-type(7) > div:nth-child(4) > div:nth-child(3) .bs-component').compareSnapshot(
+          `Indicators_Alert_Light_${breakpoint.name}`
+        )
+      })
+
+      it('Indicators: Badges', () => {
+        cy.get('.bs-docs-section:nth-of-type(7) > div:nth-child(5) .bs-component:nth-of-type(1)').compareSnapshot(
+          `Indicators_Badges_${breakpoint.name}`
+        )
+      })
+
+      it('Indicators: Badges: Pills', () => {
+        cy.get('.bs-docs-section:nth-of-type(7) > div:nth-child(5) .bs-component:nth-of-type(2)').compareSnapshot(
+          `Indicators_Badges_Pills_${breakpoint.name}`
         )
       })
     })
