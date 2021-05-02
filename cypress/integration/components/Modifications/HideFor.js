a6f7b0ff55e38aef31c21d33b4e0df7e70b7a64c
Morne Alberts <morne@malberts.net>
2021-05-02 08:41:30 +0200

    Add HideFor component tests

--- cypress/integration/components/Modifications/HideFor.js
+++ cypress/integration/components/Modifications/HideFor.js
@@ -1,1 +1,50 @@
-// TODO: Create layout
+const breakpoints = Cypress.env('breakpoints')
+
+describe(`Modification: HideFor`, () => {
+  describe('Without permission', () => {
+    before(() => {
+      cy.visit(`/wiki/Main_Page?uselayout=dirty`)
+      cy.get('.smw-entity-examiner').should('not.exist')
+    })
+
+    breakpoints.forEach((breakpoint) => {
+      let config = {
+        viewportWidth: breakpoint.width,
+        viewportHeight: breakpoint.height,
+      }
+      describe(breakpoint.name, config, () => {
+        it('NavbarHorizontal shown', () => {
+          cy.get('#mw-navigation').should('be.visible')
+        })
+
+        it('FooterInfo shown', () => {
+          cy.get('#footer-info').should('be.visible')
+        })
+      })
+    })
+  })
+
+  describe('With permission', () => {
+    before(() => {
+      cy.login()
+      cy.visit(`/wiki/Main_Page?uselayout=dirty`)
+      cy.get('.smw-entity-examiner').should('not.exist')
+    })
+
+    breakpoints.forEach((breakpoint) => {
+      let config = {
+        viewportWidth: breakpoint.width,
+        viewportHeight: breakpoint.height,
+      }
+      describe(breakpoint.name, config, () => {
+        it('NavbarHorizontal not shown', () => {
+          cy.get('#mw-navigation').should('not.exist')
+        })
+
+        it('FooterInfo not shown', () => {
+          cy.get('#footer-info').should('not.exist')
+        })
+      })
+    })
+  })
+})
