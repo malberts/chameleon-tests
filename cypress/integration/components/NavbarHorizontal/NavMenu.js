777e52257a679bfd66660c2e56a7035cf213eb14
Morne Alberts <morne@malberts.net>
2021-05-08 11:13:46 +0200

    Add initial Navbar NavMenu tests

--- cypress/integration/components/NavbarHorizontal/NavMenu.js
+++ cypress/integration/components/NavbarHorizontal/NavMenu.js
@@ -1,3 +1,26 @@
 describe('Component: NavbarHorizontal: NavMenu', () => {
-  it('Count')
+  before(() => {
+    cy.visit('/wiki/Main_Page?uselayout=navhead')
+    cy.get('.smw-entity-examiner').should('not.exist')
+  })
+
+  it('Count', () => {
+    cy.get('div[id^="n-"]').should('have.length', 4)
+  })
+
+  it('Main page', () => {
+    cy.get('#n-mainpage-description').should('be.visible')
+  })
+
+  it('Recent changes', () => {
+    cy.get('#n-recentchanges').should('be.visible')
+  })
+
+  it('Random page', () => {
+    cy.get('#n-randompage').should('be.visible')
+  })
+
+  it('Help about MediaWiki', () => {
+    cy.get('#n-help-mediawiki').should('be.visible')
+  })
 })
