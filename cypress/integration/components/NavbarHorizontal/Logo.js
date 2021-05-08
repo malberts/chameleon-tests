57737c04ea73f9971e4e581465ea1e0d473fccd6
Morne Alberts <morne@malberts.net>
2021-05-08 11:13:00 +0200

    Add initial Navbar Logo tests

--- cypress/integration/components/NavbarHorizontal/Logo.js
+++ cypress/integration/components/NavbarHorizontal/Logo.js
@@ -1,3 +1,12 @@
 describe('Component: NavbarHorizontal: Logo', () => {
-  it('Default')
+  before(() => {
+    cy.visit('/wiki/Main_Page?uselayout=navhead')
+    cy.get('.smw-entity-examiner').should('not.exist')
+  })
+
+  it('Default', () => {
+    cy.get('#p-logo img')
+      .should('have.attr', 'src', '/w/resources/assets/wiki.png')
+      .should('have.attr', 'alt', 'Chameleon Tests')
+  })
 })
