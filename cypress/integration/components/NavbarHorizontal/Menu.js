cd34a02067e545a6712f7d36b092a78530d910c4
Morne Alberts <morne@malberts.net>
2021-05-08 11:13:19 +0200

    Add initial Navbar Menu tests

--- cypress/integration/components/NavbarHorizontal/Menu.js
+++ cypress/integration/components/NavbarHorizontal/Menu.js
@@ -1,3 +1,49 @@
 describe('Component: NavbarHorizontal: Menu', () => {
-  it('Count')
+  before(() => {
+    cy.visit('/wiki/Main_Page?uselayout=navhead')
+    cy.get('.smw-entity-examiner').should('not.exist')
+  })
+
+  it('Count', () => {
+    cy.get(
+      '#mw-navigation .collapse > .navbar-nav:first-child > .nav-item'
+    ).should('have.length', 3)
+  })
+
+  it('Bootstrap', () => {
+    cy.get(
+      '#mw-navigation .collapse > .navbar-nav:first-child > .nav-item > .nav-link'
+    )
+      .contains('Bootstrap')
+      .should('be.visible')
+  })
+
+  it('Bootstrap Submenu', () => {
+    cy.get(
+      '#mw-navigation .collapse > .navbar-nav:first-child > .nav-item > .nav-link'
+    )
+      .contains('Bootstrap')
+      .click()
+    cy.get('#mw-navigation .dropdown-menu.show').within((submenu) => {
+      cy.wrap(submenu).contains('Examples')
+      cy.wrap(submenu).contains('More Examples')
+      cy.wrap(submenu).children().should('have.length', 2)
+    })
+  })
+
+  it('Chameleon', () => {
+    cy.get(
+      '#mw-navigation .collapse > .navbar-nav:first-child > .nav-item > .nav-link'
+    )
+      .contains('Chameleon')
+      .should('be.visible')
+  })
+
+  it('Testing', () => {
+    cy.get(
+      '#mw-navigation .collapse > .navbar-nav:first-child > .nav-item > .nav-link'
+    )
+      .contains('Testing')
+      .should('be.visible')
+  })
 })
