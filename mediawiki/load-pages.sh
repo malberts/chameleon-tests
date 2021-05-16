48d3db0b3564c15c23e8694a2fb63adafb3bcc0d
Morne Alberts <morne@malberts.net>
2021-05-16 11:56:32 +0200

    Updated page seeding and github extensions

--- mediawiki/load-pages.sh
+++ mediawiki/load-pages.sh
@@ -1,14 +1,24 @@
  #!/bin/sh
 
-for path in pages/**/*.txt pages/*.txt; do
-  # Remove "pages/"
-  title="${path#*pages/}"
+echo 'Load pages'
 
+find pages -name "*.txt" | while read path; do
   # Remove extension
-  title="${title%.*}"
+  title="${path%.*}"
 
-  # Insert colons
-  title=`echo $title | sed 's/\//:/'`
+  case "$title" in
+  */ns/*)
+    # Remove "pages/ns/"
+    title="${title#*pages/ns/}"
+    # Insert colons
+    title=`echo $title | sed 's/\//:/'`
+    ;;
+  *)
+    # Remove "pages/"
+    title="${title#*pages/}"
+    ;;
+  esac
 
+  echo $title
   php maintenance/edit.php --no-rc -b "$title" < "$path"
 done
